1f87cce0d7f068f32b477b6de324d1dfcbb00d4e
Hanish Kumar <er.hanishahir@gmail.com>
2025-01-27 17:44:14 -0400

    home page form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,6 +19,6 @@ class HomeController extends Controller
     public function emailEnquiry(Request $mailData){
 
         Mail::to('ohcinc22@gmail.com')->send(new HomepageEmailForm($mailData));
-        return true;
+        return view('index');
     }
 }
